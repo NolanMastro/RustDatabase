61df63acd92623eb3094a7348d7f7257124cb00c
NolanMastro <nolandmastro@gmail.com>
2024-12-16 13:54:28 -0800

    add items.py

---
+++ items.py
@@ -0,0 +1,404 @@
+Weapons
+
+    8x Zoom Scope
+    Assault Rifle
+    Beancan Grenade
+    Bolt Action Rifle
+    Bone Club
+    Bone Knife
+    Butcher Knife
+    Candy Cane Club
+    Combat Knife
+    Compound Bow
+    Crossbow
+    Custom SMG
+    Double Barrel Shotgun
+    Eoka Pistol
+    Extended Magazine
+    F1 Grenade
+    Flame Thrower
+    Flashbang
+    Gas Compression Overdrive
+    HMLMG
+    Handmade SMG
+    High Caliber Revolver
+    Holosight
+    Homing Missile Launcher
+    Hunting Bow
+    L96 Rifle
+    LR-300 Assault Rifle
+    M249
+    M39 Rifle
+    M4 Shotgun
+    M92 Pistol
+    MP5A4
+    Mace
+    Machete
+    Military Flame Thrower
+    Minigun
+    Molotov Cocktail
+    Multiple Grenade Launcher
+    Muzzle Boost
+    Muzzle Brake
+    Nailgun
+    Paddle
+    Prototype 17
+    Pump Shotgun
+    Python Revolver
+    Revolver
+    Rocket Launcher
+    SKS
+    Salvaged Cleaver
+    Salvaged Sword
+    Semi-Automatic Pistol
+    Semi-Automatic Rifle
+    Silencer
+    Simple Handmade Sight
+    Snowball
+    Snowball Gun
+    Spas-12 Shotgun
+    Speargun
+    Thompson
+    16x Scope
+    Waterpipe Shotgun
+    Weapon Lasersight
+    Weapon Flashlight
+    Wooden Spear
+
+Construction
+
+    Armored Door
+    Armored Double Door
+    Barbed Wooden Barricade
+    Building Plan
+    Chainlink Fence
+    Chainlink Fence Gate
+    Code Lock
+    Concrete Barricade
+    Door Closer
+    Floor Grill
+    Floor Triangle Grill
+    Garage Door
+    High External Stone Gate
+    High External Stone Wall
+    High External Wooden Gate
+    High External Wooden Wall
+    High Ice Wall
+    Key Lock
+    Ladder Hatch
+    Large Water Catcher
+    Legacy Wood Shelter
+    Metal Barricade
+    Metal Shop Front
+    Metal Vertical Embrasure
+    Metal Window Bars
+    Metal Horizontal Embrasure
+    Mining Quarry
+    Netting
+    Prison Cell Gate
+    Prison Cell Wall
+    Pump Jack
+    Reinforced Glass Window
+    Sandbag Barricade
+    Sheet Metal Door
+    Sheet Metal Double Door
+    Shop Front
+    Short Ice Wall
+    Small Water Catcher
+    Stone Barricade
+    Strengthened Glass Window
+    Tool Cupboard
+    Triangle Ladder Hatch
+    Watch Tower
+    Wood Double Door
+    Wood Shutters
+    Wooden Barricade
+    Wooden Barricade Cover
+    Wooden Door
+    Wooden Frontier Bar Doors
+    Wooden Ladder
+    Wooden Window Bars
+
+Deployables
+
+    Barbeque
+    Bed
+    Bota Bag
+    Camp Fire
+    Carvable Pumpkin
+    Chair
+    Chinese Lantern
+    Chippy Arcade Game
+    Christmas Door Wreath
+    Christmas Lights
+    Christmas Tree
+    Clan Table
+    Composter
+    Diver Propulsion Vehicle
+    Double Sign Post
+    Dragon Door Knocker
+    Drone
+    Drop Box
+    Easter Door Wreath
+    Festive Doorway Garland
+    Fish Trophy
+    Frankenstein Table
+    Fridge
+    Furnace
+    Hazmat Plushy
+    Hazmat Youtooz
+    Heavy Scientist Youtooz
+    Hitch & Trough
+    Hobo Barrel
+    Horizontal Weapon Rack
+    Huge Wooden Sign
+    Ice Sculpture
+    Jack O Lantern Angry
+    Jack O Lantern Happy
+    Kayak
+    Landscape Photo Frame
+    Landscape Picture Frame
+    Lantern
+    Large Banner Hanging
+    Large Furnace
+    Large Hunting Trophy
+    Large Photo Frame
+    Large Planter Box
+    Large Wood Box
+    Large Wooden Sign
+    Locker
+    Mail Box
+    Medium Wooden Sign
+    Minecart Planter
+    Mixing Table
+    One Sided Town Sign Post
+    Paper Map
+    Pookie Bear
+    Portrait Photo Frame
+    Rail Road Planter
+    Repair Bench
+    Research Table
+    Rocking Chair
+    Rug
+    Rug Bear Skin
+    Scarecrow
+    Secretlab Chair
+    Single Sign Post
+    Skull Door Knocker
+    Skull Fire Pit
+    Sleeping Bag
+    Small Hunting Trophy
+    Small Oil Refinery
+    Small Planter Box
+    Small Stash
+    Small Wooden Sign
+    Spinning Wheel
+    Stone Fireplace
+    Storage Barrel Horizontal
+    Survival Fish Trap
+    Table
+    Tall Picture Frame
+    Tall Weapon Rack
+    Torch Holder
+    Tuna Can Lamp
+    Twitch Rivals Trophy
+    Two Sided Hanging Sign
+    Vending Machine
+    Wallpaper
+    Wanted Poster
+    Water Barrel
+    Water Purifier
+    Weapon Rack Stand
+    Wide Weapon Rack
+    Wood Storage Box
+    Workbench Level 1
+    Workbench Level 2
+    Workbench Level 3
+    XL Picture Frame
+    XXL Picture Frame
+
+Resources
+
+    Animal Fat
+    Battery - Small
+    Bone Fragments
+    CCTV Camera
+    Charcoal
+    Cloth
+    Coal
+    Crude Oil
+    Diesel Fuel
+    Empty Can Of Beans
+    Empty Tuna Can
+    Explosives
+    Fertilizer
+    Gun Powder
+    High Quality Metal
+    High Quality Metal Ore  
+    Leather
+    Low Grade Fuel
+    Metal Fragments
+    Metal Ore
+    Paper
+    Plant Fiber
+    Radioactive Water
+    Research Paper
+    Salt Water
+    Scrap
+    Stones
+    Sulfur
+    Sulfur Ore
+    Targeting Computer
+    Water
+    Wolf Skull
+    Wood
+
+Attire
+
+    Bandana Mask
+    Baseball Cap
+    Basic Horse Shoes
+    Beanie Hat
+    Bone Armor
+    Bone Helmet
+    Boonie Hat
+    Boots
+    Bucket Helmet
+    Burlap Gloves
+    Burlap Headwrap
+    Burlap Shirt
+    Burlap Shoes
+    Burlap Trousers
+    Candle Hat
+    Card Movember Moustache
+    Chicken Costume
+    Clatter Helmet
+    Coffee Can Helmet
+    Crate Costume
+    Diving Fins
+    Diving Mask
+    Diving Tank
+    Double Horse Saddle
+    Dracula Cape
+    Dracula Mask
+    Dragon Mask
+    Egg Suit
+    Frankenstein Mask
+    Frankenstein Suit
+    Frog Boots
+    Gas Mask
+    Ghost Costume
+    Gingerbread Suit
+    Glowing Eyes
+    Hazmat Suit
+    Headset
+    Heavy Frankenstein Head
+    Heavy Frankenstein Legs
+    Heavy Frankenstein Torso
+    Heavy Plate Helmet
+    Heavy Plate Jacket
+    Heavy Plate Pants
+    Heavy Scientist Suit
+    Hide Boots
+    Hide Halterneck
+    Hide Pants
+    Hide Poncho
+    Hide Skirt
+    Hide Vest
+    High Quality Horse Shoes
+    Hoodie
+    Horse Saddle
+    Hot Air Balloon Armor
+    Improvised Balaclava
+    Jacket
+    Jumpsuit
+    Large Backpack
+    Leather Gloves
+    Light Frankenstein Head
+    Light Frankenstein Legs
+    Light Frankenstein Torso
+    Longsleeve T-Shirt
+    Lumberjack Hoodie
+    Medium Frankenstein Head
+    Medium Frankenstein Legs
+    Medium Frankenstein Torso
+    Metal Chest Plate
+    Metal Facemask
+    Miners Hat
+    Movember Moustache
+    Mummy Mask
+    Mummy Suit
+    NVGM Scientist Suit
+    Night Vision Goggles
+    Ninja Suit
+    Ox Mask
+    Pants
+    Parachute
+    Party Hat
+    Prisoner Hood
+    Purple Sunglasses
+    Rabbit Mask
+    Rat Mask
+    Reindeer Antlers
+    Riot Helmet
+    Road Sign Jacket
+    Road Sign Kilt
+    Roadsign Gloves
+    Roadsign Horse Armor
+    Saddle Bag
+    Santa Beard
+    Santa Hat
+    Scarecrow Suit
+    Scientist Suit
+    Shirt
+    Shorts
+    Single Horse Saddle
+    Small Backpack
+    Snow Jacket
+    Snowman Helmet
+    Sunglasses
+    Surgeon Scrubs
+    T-Shirt
+    Tactical Gloves
+    Tank Top
+    Tiger Mask
+    Twitch Rivals Flag
+    Waterwell NPC Jumpsuit
+    Wetsuit
+    Wolf Headdress
+    Wood Armor Helmet
+    Wood Armor Pants
+    Wood Chestplate
+    Wooden Horse Armor
+
+Tools
+
+    Binoculars
+    Birthday Cake
+    Camera
+    Chainsaw
+    Fishing Tackle
+    Flare
+    Flashlight
+    Hammer
+    Handcuffs
+    Handmade Fishing Rod
+    Hatchet
+    Instant Camera
+    Jackhammer
+    Metal Detector
+    Pickaxe
+    RF Transmitter
+    Rock
+    Salvaged Axe
+    Salvaged Hammer
+    Salvaged Icepick
+    Satchel Charge
+    Shovel
+    Smoke Grenade
+    Spray Can
+    Stone Hatchet
+    Stone Pickaxe
+    Supply Signal
+    Survey Charge
+    Timed Explosive Charge
