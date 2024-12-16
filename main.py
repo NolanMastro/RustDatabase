323742f728f5a4d60fca967b5dc350562bee7ab0
NolanMastro <nolandmastro@gmail.com>
2024-12-16 14:01:59 -0800

    Add items.json for comprehensive item data; removed items.py. Introduced main.py for future resource management features.

---
+++ main.py
@@ -0,0 +1,4 @@
+#IDEA - website that takes in screenshots of all item containers/large boxes in your base. It adds up total amount of resources and is able to return what you can craft, what your missing, have a excess of, etc.
+
+import json
+
